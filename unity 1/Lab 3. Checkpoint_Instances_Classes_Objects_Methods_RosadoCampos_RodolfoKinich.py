e18e52c32b701084f5a6dfb111e3560c2889fbcb
Rodolfo-poker <rodolfo_3142250032@utd.edu.mx>
2026-09-21 18:31:11 -0600

    Laboratory 3: Checkpoint - Instances, classes, objects & methods (Rosado Campos Rodolfo Kinich)

---
+++ unity 1/Lab 3. Checkpoint_Instances_Classes_Objects_Methods_RosadoCampos_RodolfoKinich.py
@@ -0,0 +1,29 @@
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+    
+    def introduce(self):
+        print(f"HI, my name is {self.name}")
+
+class Post:
+    def __init__(self, title, content, author):
+        self.title = title
+        self.content = content
+        self.author = author
+    
+    def show_post(self):
+        print(f"Title: {self.title}")
+        print(f"Author: {self.author}")
+        print(f"Content: {self.content}")
+        
+user1 = User("Carlos", "carlos@email.com")
+
+post1 = Post(
+    "My first post",
+    "I am learning Python!",
+    user1
+)
+
+post1.show_post()
+print(post1.author.name)
